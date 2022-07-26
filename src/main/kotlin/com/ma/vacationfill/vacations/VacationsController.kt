ddf4810c152faa2b4e1d18c6cc2e88885d0c383f
Muhammad Adam <muhammadalyadam@gmail.com>
2022-07-26 02:51:51 +0200

    update vacations APIs accept, reject and cancel to have the vacationId in the path

--- src/main/kotlin/com/ma/vacationfill/vacations/VacationsController.kt
+++ src/main/kotlin/com/ma/vacationfill/vacations/VacationsController.kt
@@ -1,6 +1,7 @@
 package com.ma.vacationfill.vacations
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -17,19 +18,19 @@ class VacationsController(private val vacationsService: VacationsService) {
         vacationsService.submitVacation(vacation)
     }
 
-    @PostMapping(path = ["api/v1/vacations/accept"])
-    fun acceptVacation(@RequestBody bodyDto: VacationAcceptanceRequestBodyDto) {
-        vacationsService.acceptVacation(bodyDto.vacationId)
+    @PostMapping(path = ["api/v1/vacations/{vacationId}/accept"])
+    fun acceptVacation(@PathVariable vacationId: Long) {
+        vacationsService.acceptVacation(vacationId)
     }
 
-    @PostMapping(path = ["api/v1/vacations/reject"])
-    fun rejectVacation(@RequestBody bodyDto: VacationAcceptanceRequestBodyDto) {
-        vacationsService.rejectVacation(bodyDto.vacationId)
+    @PostMapping(path = ["api/v1/vacations/{vacationId}/reject"])
+    fun rejectVacation(@PathVariable vacationId: Long) {
+        vacationsService.rejectVacation(vacationId)
     }
 
-    @PostMapping(path = ["api/v1/vacations/cancel"])
-    fun cancelVacation(@RequestBody bodyDto: VacationAcceptanceRequestBodyDto) {
-        vacationsService.cancelVacation(bodyDto.vacationId)
+    @PostMapping(path = ["api/v1/vacations/{vacationId}/cancel"])
+    fun cancelVacation(@PathVariable vacationId: Long) {
+        vacationsService.cancelVacation(vacationId)
     }
 }
 
